eb8657c303d674beb8f6e2443a9fb131f1df835d
meimo <59273253+meimodev@users.noreply.github.com>
2025-04-14 15:52:34 +0800

    initial commit

---
+++ src/pages/2/index.tsx
@@ -0,0 +1,20 @@
+
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Title Goes Here',
+    description: 'Description',
+
+}
+export default function Home() {
+
+    return   (
+        <div>
+            Second Ebook
+        </div>
+    )
+}
+
+
+
+
